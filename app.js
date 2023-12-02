51c5115c47d1b8a626d70a144e1a6951ce84ee62
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-02 12:53:12 +0100

    Add primary JS logic for the HBS theme.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
-
 const path = require('path')
+
 app.use('/assets', express.static(path.join(__dirname, "./assets")));
+app.use('/js', express.static(path.join(__dirname, "./js")));
 
 app.set("view engine", 'hbs');
 
