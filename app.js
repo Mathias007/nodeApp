4e15c8fa27caf4e40d8cdba8bbbeeeaf804df678
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-02 13:02:54 +0100

    Add dynamic text rendering on the website.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
-const path = require('path')
+const path = require('path');
+const fromAnotherFile = require("./js/dynamic")
 
 app.use('/assets', express.static(path.join(__dirname, "./assets")));
 app.use('/js', express.static(path.join(__dirname, "./js")));
@@ -10,7 +11,15 @@ app.use('/js', express.static(path.join(__dirname, "./js")));
 app.set("view engine", 'hbs');
 
 app.get('/', function (req, res) {
-    res.render('index');
+        const clickLabel = () => {
+            return "Klik";
+        }
+
+        res.render('index', {
+        pageTitle: "Start Page",
+        subTitle: fromAnotherFile.someTitle,
+        clickLabel: clickLabel()
+    });
 });
 
 app.get('/about', function (req, res){
---
+++ js/dynamic.js
@@ -0,0 +1,15 @@
+const sayHello = () => {
+    console.log("Hello World") 
+};
+
+const add = (a, b) => {
+    console.log(a + b);
+}
+
+const someTitle = "First Subtitle";
+
+module.exports = {
+    sayHello,
+    add,
+    someTitle 
+};
